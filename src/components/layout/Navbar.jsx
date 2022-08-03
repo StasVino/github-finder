b58a305e7738d70bfbdadb4f01fa1528a015201c
unknown <st13vv@gmail.com>
2022-08-03 20:20:36 +0300

    Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,34 @@
-function Navbar() {
-  return <div>Navbar</div>;
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Navbar({ title }) {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 px-2">
+          <FaGithub className="'inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <flex className="justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link>
+          </flex>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
 
 export default Navbar;
