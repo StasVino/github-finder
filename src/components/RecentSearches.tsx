744729065549881c7f740be9795d417162a6e04d
unknown <st13vv@gmail.com>
2025-12-28 20:28:36 +0200

    added prefetch and local storage

--- src/components/RecentSearches.tsx
+++ src/components/RecentSearches.tsx
@@ -1,10 +1,15 @@
 import { FaClock, FaUser } from 'react-icons/fa';
+import { useQueryClient } from '@tanstack/react-query';
+import { fetchGithubUser } from '../api/github';
+
 type RecentSearchesProps = {
   users: string[];
   onSelect: (username: string) => void;
 };
 
 const RecentSearches = ({ users, onSelect }: RecentSearchesProps) => {
+  const queryClient = useQueryClient();
+
   return (
     <div className="recent-searches">
       <div className="recent-header">
@@ -14,7 +19,15 @@ const RecentSearches = ({ users, onSelect }: RecentSearchesProps) => {
       <ul>
         {users.map((user) => (
           <li key={user}>
-            <button onClick={() => onSelect(user)}>
+            <button
+              onClick={() => onSelect(user)}
+              onMouseEnter={() => {
+                queryClient.prefetchQuery({
+                  queryKey: ['users', user],
+                  queryFn: () => fetchGithubUser(user),
+                });
+              }}
+            >
               <FaUser className="user-icon" />
               {user}
             </button>
