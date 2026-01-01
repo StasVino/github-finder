48a15c66d64a5f5860c047078e2d27a34d0e6b01
unknown <st13vv@gmail.com>
2026-01-01 20:40:54 +0200

    added follow buttom

--- src/api/github.ts
+++ src/api/github.ts
@@ -17,3 +17,25 @@ export const searchGithubUser = async (query: string) => {
   const data = await res.json();
   return data.items;
 };
+
+// Check if following user on githhub
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      headers: {
+        Authorazation: `Bearer ${import.meta.env.VITE_GITHHUB_API_TOKEN}`,
+        Accept: 'application/vnd.github+json',
+      },
+    }
+  );
+
+  if (res.status === 204) {
+    return true;
+  } else if (res.status === 404) {
+    return false;
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData.message || 'Failed to check follow status');
+  }
+};
